42498f299c8cfcbe5f97e303085f736e67fb19e9
engmustafasabah <eng.m.sabah@gmail.com>
2023-11-23 22:17:42 +0200

    init 3d website

---
+++ src/components/Version/index.tsx
@@ -0,0 +1,5 @@
+import { Color } from 'three';
+import Style from './Version.module.scss';
+
+// export const Version = () => <aside class={Style.version}>v{VERSION}</aside>;
+export const Version = () => <aside class={Style.version}>Copyrights 2023. All Rights are Reserved by <a href="https://www.linkedin.com/in/mustafa-sabah/" style="Color: #055; font-size:20px" target="_blank" rel="noopener noreferrer">Mustafa Sabah</a> v{1.1}</aside>;
